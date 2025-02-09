dc3274bf2661c1cd6f0586242331bff7890c5e1c
dineshkanth2005 <dineshkanth2005@gmail.com>
2025-02-09 16:28:59 +0530

    Update csvParser.ts

--- utils/csvParser.ts
+++ utils/csvParser.ts
@@ -1,30 +1,36 @@
-import Papa from "papaparse"
+import Papa from 'papaparse';
 
 export interface PictureChartItem {
-  name: string
-  slNo: string
+  name: string;
+  slNo: string;
+}
+
+interface ParseError {
+  type: string;
+  code: string;
+  message: string;
+  row?: number;
 }
 
 export async function fetchAndParseCsv(url: string): Promise<PictureChartItem[]> {
-  const response = await fetch(url)
-  const csvText = await response.text()
-
+  const response = await fetch(url);
+  const csvText = await response.text();
+  
   return new Promise((resolve, reject) => {
     Papa.parse(csvText, {
       header: true,
       complete: (results) => {
         const items = results.data
-          .filter((item: any) => item["Picture Chart Name"] && item["Sl. No"])
+          .filter((item: any) => item['Picture Chart Name'] && item['Sl. No'])
           .map((item: any) => ({
-            name: item["Picture Chart Name"],
-            slNo: item["Sl. No"],
-          }))
-        resolve(items)
+            name: item['Picture Chart Name'],
+            slNo: item['Sl. No']
+          }));
+        resolve(items);
       },
-      error: (error) => {
-        reject(error)
-      },
-    })
-  })
+      error: (error: ParseError) => {
+        reject(error);
+      }
+    });
+  });
 }
-
